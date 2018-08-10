6c213d56fd4ec9d32d6374546a1ed8d6e69d0607
Joe Reddington <joe@joereddington.com>
2018-08-10 16:18:44 +0100

    test passing

--- igor.py
+++ igor.py
@@ -1,6 +1,12 @@
+import csv
 
+events=[]
 
 
 
 def import_events(filename):
-    pass
+    with open(filename, 'rU') as events_file:
+        reader = csv.reader(events_file, skipinitialspace=True)
+        lines = filter(None, reader)
+        for line in lines:
+            events.append(line)
--- test_igor.py
+++ test_igor.py
@@ -7,7 +7,7 @@ class igor_test(TestCase):
     def test_read_file(self):
        "only checking if it reads the right size"
        igor.import_events("testinputs/events.csv")
-       self.assertequal(len(igor.events),2)
+       self.assertEqual(len(igor.events),2)
 
 if __name__=="__main__":
     unittest.main()
