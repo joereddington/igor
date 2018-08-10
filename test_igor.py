bb1a4ad30a92cad5554f63cf0664cf5405e0d823
Joe Reddington <joe@joereddington.com>
2018-08-10 16:20:07 +0100

    Second test unexpectedly passes

--- test_igor.py
+++ test_igor.py
@@ -9,5 +9,9 @@ class igor_test(TestCase):
        igor.import_events("testinputs/events.csv")
        self.assertEqual(len(igor.events),2)
 
+    def test_read_file_and_get_priority(self):
+       "only checking if it reads the right size"
+       igor.import_events("testinputs/events.csv")
+       self.assertEqual(int(igor.events[0][0]),1)
 if __name__=="__main__":
     unittest.main()
