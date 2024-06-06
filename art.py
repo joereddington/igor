76cbe52d58c0fc8173e82cca9f30b120f5c8b325
Joe Reddington <joe@equalitytime.co.uk>
2024-06-06 10:30:34 +0100

    First steps in building a project review system

--- art.py
+++ art.py
@@ -3,7 +3,6 @@ import os
 import datetime
 import argparse
 
-events=[]
 args={}
 here= os.path.dirname(os.path.realpath(__file__))
 
@@ -11,17 +10,16 @@ def setup_argument_list():
     "creates and parses the argument list for ART (Add reoccuring tasks)"
     parser = argparse.ArgumentParser( description="manages Igor")
     parser.add_argument('-n', action="store_true", help="Show only tasks since the last recorded run")
-#    parser.add_argument('-d', action="store_true", help="Show only tasks since this date")
     parser.set_defaults(verbatim=False)
     return parser.parse_args()
 
-def tasks_since(start,end=datetime.date.today().toordinal()):
+def tasks_since(events,start,end=datetime.date.today().toordinal()):
     return_list=[]
     for i in range(start,end):
-        return_list.extend(tasks_on_date(i))
+        return_list.extend(tasks_on_date(events,i))
     return return_list
     
-def tasks_on_date(target_date):
+def tasks_on_date(events,target_date):
     ordinal=0
     return_list=[]
     if isinstance(target_date,int):
@@ -34,47 +32,38 @@ def tasks_on_date(target_date):
     return return_list
 
 
-def generate_list(days,start=0):
-    return_list=[]
-    for i in range(start,start+days):
-        for event in events:
-            if i % int(event[0]) ==0:
-                return_list.append(event[1])
-    return return_list
-
-
 def import_events(filename):
+    events=[]
     with open(filename, 'rU') as events_file:
         reader = csv.reader(events_file, skipinitialspace=True)
         lines = [_f for _f in reader if _f]
         for line in lines:
             events.append(line)
-    #TODO - this should return the set of events (which should not be global) 
+    return events
 
-def print_recent_tasks(number_of_days_to_go_back=4):
+def print_recent_tasks(events,number_of_days_to_go_back=4):
     for i in range(number_of_days_to_go_back+1):
         print("")
         print((datetime.date.today()-datetime.timedelta(number_of_days_to_go_back-i)))
-        for task in tasks_on_date(datetime.date.today()-datetime.timedelta(number_of_days_to_go_back-i)):
+        for task in tasks_on_date(events,datetime.date.today()-datetime.timedelta(number_of_days_to_go_back-i)):
             print(task)
 
-#TODO - rename these two functions so it is clear what they do 
-def read_integers():
+def read_last_date():
     with open(here+"/art_lastdate.txt") as f:
         return list(map(int, f))
 
-def write_ordinal(): #Todo what is an ordinal here
+def write_last_date(): #Todo what is an ordinal here
    f=open(here+"/art_lastdate.txt","w")
    f.write("{}".format(datetime.date.today().toordinal()))
    f.close()
 
 def go(): #? Where is this called? 
-    import_events(here+"/art_events.csv") # Read the events - okay, but then what are they used for? 
+    events=import_events(here+"/art_events.csv") # Read the events - okay, but then what are they used for? 
     if args.n:
-        for task in tasks_since(read_integers()[0]):
+        for task in tasks_since(read_last_date()[0]):
             print(task)
-        write_ordinal() #TODO why is it write_ordinal? The arguments suggests that we don't write the oridinal unless commiting... 
+        write_last_date()
     else:
         print("ART V0.1")
         print("Current ordinal is: {}".format(datetime.date.today().toordinal()))
-        print_recent_tasks()
+        print_recent_tasks(events)
--- test_art.py
+++ test_art.py
@@ -2,70 +2,57 @@ from unittest import TestCase
 import unittest
 import datetime
 import art
+import how_long_to_complete_a_todo_list
 
 class art_test(TestCase):
 
     def test_read_file(self):
        "only checking if it reads the right size"
-       art.events=[]
-       art.import_events("testinputs/events.csv")
-       self.assertEqual(len(art.events),2)
+       events=art.import_events("testinputs/events.csv")
+       self.assertEqual(len(events),2)
 
     def test_read_file_and_get_priority(self):
        "only checking if it reads the right size"
-       art.events=[]
-       art.import_events("testinputs/events.csv")
-       self.assertEqual(int(art.events[0][0]),1)
-
-
-    def test_events_in_ten_days(self):
-       art.events=[]
-       art.import_events("testinputs/events.csv")
-       events=art.generate_list(10)
-       self.assertEqual(len(events),12)
-
+       events=art.import_events("testinputs/events.csv")
+       self.assertEqual(int(events[0][0]),1)
 
 
     def test_events_on_this_day_1(self):
         current_date = datetime.date.today()
         target_date=datetime.date.fromordinal(105)
-        art.events=[]
-        art.import_events("testinputs/events.csv")
-        tasks=art.tasks_on_date(target_date)
+        events=art.import_events("testinputs/events.csv")
+        tasks=art.tasks_on_date(events,target_date)
         self.assertEqual(len(tasks),2)
 
     def test_events_on_this_day_2(self):
         current_date = datetime.date.today()
         target_date=datetime.date.fromordinal(100)
-        art.events=[]
-        art.import_events("testinputs/events.csv")
-        tasks=art.tasks_on_date(target_date)
+        events=art.import_events("testinputs/events.csv")
+        tasks=art.tasks_on_date(events,target_date)
         self.assertEqual(len(tasks),1)
     
     def test_get_events_for_this_ordinal(self):
         ordinal=7
-        tasks=art.tasks_on_date(ordinal)
+        events=art.import_events("testinputs/events.csv")
+        tasks=art.tasks_on_date(events,ordinal)
         self.assertEqual(len(tasks),2)
 
     def test_tasks_since(self):
         start=7
         end=15
-        tasks=art.tasks_since(start,end)
+        events=art.import_events("testinputs/events.csv")
+        tasks=art.tasks_since(events,start,end)
         self.assertEqual(len(tasks),10)
 
     def test_extract_number(self):
         #TODO - write the code that goes with this   
         input_string = ") is 42 and the second number is 1234"
         expected_output = 42
-        assert extract_number(input_string) == expected_output
-
-        input_string = "The number is 1"
-        expected_output = 1
-        assert extract_number(input_string) == expected_output
+        assert how_long_to_complete_a_todo_list.find_two_digit_number(input_string) == expected_output
 
         input_string = "No number found"
         expected_output = None
-        assert extract_number(input_string) == expected_output
+        assert how_long_to_complete_a_todo_list.find_two_digit_number(input_string) == expected_output
 
 #I'll be reading in a list of projects and checking to see if they are in the list of tasks. It's NOT actually hard right?  
 
