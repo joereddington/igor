75cbe15c5e87f265c16597cf26083fa92bc1055f
Joe Reddington <joe@equalitytime.co.uk>
2024-10-03 11:02:54 +0100

    update before fix

--- igor_print.py
+++ igor_print.py
@@ -66,10 +66,13 @@ if __name__ == "__main__":
     script_path = os.path.dirname(os.path.abspath(__file__))+"/"
 
     database=TaskDatabase(script_path+"databases/database.json") #Main database
+    print("Todo database loaded")
     database.update_current_tasks(get_todo_list(script_path+"../todo.txt/todo.txt"))
     rhul_database=TaskDatabase(script_path+"databases/rhul.database.json")
+    print("rhul database loaded")
     rhul_database.update_current_tasks(get_todo_list(script_path+"../diary/rhul.todo.txt"))
     eqt_database=TaskDatabase(script_path+"databases/eqt.database.json")
+    print("eqt database loaded")
     eqt_database.update_current_tasks(get_todo_list(script_path+"../todo.txt/eqt.todo.txt"))
 
     display(database,script_path+"outputs/results.txt")
